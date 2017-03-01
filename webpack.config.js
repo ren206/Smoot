4933a74867fdda72cc4bf6237f301ee5b7ada33c
Michael Ren <michael.ren73@gmail.com>
2017-03-01 16:17:45 -0500

    Install webpack and add a canvas

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+module.exports = {
+ context: __dirname,
+ entry: './lib/smoot.js',
+ output: {
+   path: './',
+   filename: 'bundle.js'
+ },
+ resolve: {
+   extensions: ['.js', '.jsx', '*']
+ },
+ module: {
+   loaders: [
+     {
+       test: /\.jsx?$/,
+       exclude: /(node_modules|bower_components)/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['es2015']
+       }
+     }
+   ]
+ },
+ devtool: 'source-maps'
+};
