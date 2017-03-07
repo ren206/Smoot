80537bed4e22d644cb09a4423b15d6e1a698248f
Michael Ren <michael.ren73@gmail.com>
2017-03-07 12:30:54 -0500

    Add face

--- bundle.js
+++ bundle.js
@@ -144,6 +144,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _my_canvas = __webpack_require__(1);
@@ -176,6 +178,8 @@ var Smoot = function () {
     this.gridPos = options.gridPos;
     this.radius = options.radius || _settings2.default.SMOOT.RADIUS;
     this.vel = options.vel || [0, 0];
+
+    this.happy = Math.random() < 0.5 ? true : false;
   }
 
   _createClass(Smoot, [{
@@ -219,6 +223,33 @@ var Smoot = function () {
       ctx.arc(this.centerPos[0] + this.radius / 2, this.centerPos[1] - this.radius / 2, 4, 0, Math.PI * 2);
       ctx.closePath();
       ctx.fill();
+
+      // nose
+
+      ctx.beginPath();
+
+      ctx.arc(this.centerPos[0], this.centerPos[1], 4, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.fill();
+
+      // mouth
+      var mouthStart = void 0,
+          mouthEnd = void 0,
+          centerOffset = void 0;
+
+      var _ref = this.happy ? [0, Math.PI, this.radius / 4] : [Math.PI, 0, this.radius / 1.25];
+
+      var _ref2 = _slicedToArray(_ref, 3);
+
+      mouthStart = _ref2[0];
+      mouthEnd = _ref2[1];
+      centerOffset = _ref2[2];
+
+      ctx.beginPath();
+      ctx.arc(this.centerPos[0], this.centerPos[1] + centerOffset, this.radius / 2, mouthStart, mouthEnd);
+      ctx.closePath();
+      ctx.lineWidth = 2;
+      ctx.fillStyle = "black", ctx.fill();
     }
   }, {
     key: 'matchesWith',
