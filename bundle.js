a16c5fde0670070efe2b21becd9bab42961e3612
Michael Ren <michael.ren73@gmail.com>
2017-03-22 18:46:19 -0400

    Update floating logic

--- bundle.js
+++ bundle.js
@@ -147,11 +147,11 @@ var _settings = __webpack_require__(0);
 
 var _settings2 = _interopRequireDefault(_settings);
 
-var _cannon = __webpack_require__(5);
+var _cannon = __webpack_require__(6);
 
 var _cannon2 = _interopRequireDefault(_cannon);
 
-var _board = __webpack_require__(4);
+var _board = __webpack_require__(5);
 
 var _board2 = _interopRequireDefault(_board);
 
@@ -193,7 +193,6 @@ var Game = function () {
                 return;
               }
               this.handleMatches();
-              // this.handleFloatingGroups();
               this.reload();
               if (this.board.isEmpty()) this.winGame();
             }
@@ -230,21 +229,16 @@ var Game = function () {
       return _settings2.default.SMOOT.COLORS[Math.floor(Math.random() * numColors)];
     }
   }, {
-    key: 'handleFloatingGroups',
-    value: function handleFloatingGroups() {
-      var _this = this;
-
-      var floatingGroups = this.board.findFloatingGroups();
-      if (floatingGroups.length > 0) floatingGroups.forEach(function (floater) {
-        return _this.drop(floater);
-      });
-      this.resetChecks();
-    }
-  }, {
     key: 'handleMatches',
     value: function handleMatches() {
       var matches = this.board.findNeighboringSmootMatches(this.smoot);
-      if (matches.length > 2) this.drop(matches);
+      if (matches.length > 2) {
+        this.drop(matches);
+        this.resetChecks();
+
+        // const floatingGroups = this.board.findFloatingGroups();
+        // if (floatingGroups.length > 0) floatingGroups.forEach(floater => this.drop(floater));
+      }
       this.resetChecks();
     }
   }, {
@@ -347,7 +341,7 @@ var _settings = __webpack_require__(0);
 
 var _settings2 = _interopRequireDefault(_settings);
 
-var _utils = __webpack_require__(8);
+var _utils = __webpack_require__(3);
 
 var Utils = _interopRequireWildcard(_utils);
 
@@ -504,6 +498,20 @@ exports.default = Smoot;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var getDistanceBetween2 = exports.getDistanceBetween2 = function getDistanceBetween2(pos1, pos2) {
+  return Math.sqrt(Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[1] - pos2[1], 2));
+};
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -596,7 +604,7 @@ var GameView = function () {
 exports.default = GameView;
 
 /***/ }),
-/* 4 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -614,7 +622,7 @@ var _smoot = __webpack_require__(2);
 
 var _smoot2 = _interopRequireDefault(_smoot);
 
-var _smoot_space = __webpack_require__(7);
+var _smoot_space = __webpack_require__(8);
 
 var _smoot_space2 = _interopRequireDefault(_smoot_space);
 
@@ -622,7 +630,7 @@ var _settings = __webpack_require__(0);
 
 var _settings2 = _interopRequireDefault(_settings);
 
-var _utils = __webpack_require__(8);
+var _utils = __webpack_require__(3);
 
 var Utils = _interopRequireWildcard(_utils);
 
@@ -839,8 +847,32 @@ var Board = function () {
   }, {
     key: 'findFloaters',
     value: function findFloaters(smoot) {
-      var floaters = [];
+      var _this3 = this;
 
+      var floaters = [];
+      var potentialCluster = [smoot];
+      var currentSmoot = void 0;
+
+      while (potentialCluster.length > 0) {
+        currentSmoot = potentialCluster.pop();
+        if (!currentSmoot.isChecked) {
+          if (currentSmoot.gridPos[0] === 0) return [];
+          floaters.push(currentSmoot);
+        }
+
+        currentSmoot.isChecked = true;
+        var neighborGridPositions = this.getNeighborGridPositions(currentSmoot.gridPos);
+        var smootNeighborPositions = neighborGridPositions.filter(function (pos) {
+          return _this3.grid[pos[0]][pos[1]] instanceof _smoot2.default;
+        });
+        smootNeighborPositions.forEach(function (smootNeighborPos) {
+          var neighborSmoot = _this3.grid[smootNeighborPos[0]][smootNeighborPos[1]];
+
+          if (!neighborSmoot.isChecked) {
+            potentialCluster.push(neighborSmoot);
+          }
+        });
+      }
       return floaters;
     }
 
@@ -862,7 +894,7 @@ var Board = function () {
   }, {
     key: 'findNeighboringSmootMatches',
     value: function findNeighboringSmootMatches(smoot) {
-      var _this3 = this;
+      var _this4 = this;
 
       var matchingSmoots = [smoot];
       var smootGridPos = smoot.gridPos;
@@ -871,19 +903,19 @@ var Board = function () {
 
       // ensure the neighbors are Smoots
       var smootNeighborPositions = neighborGridPositions.filter(function (pos) {
-        return _this3.grid[pos[0]][pos[1]] instanceof _smoot2.default;
+        return _this4.grid[pos[0]][pos[1]] instanceof _smoot2.default;
       });
 
       // filter for neighbor smoots
       smootNeighborPositions.forEach(function (smootNeighborPos) {
-        var neighborSmoot = _this3.grid[smootNeighborPos[0]][smootNeighborPos[1]];
+        var neighborSmoot = _this4.grid[smootNeighborPos[0]][smootNeighborPos[1]];
 
         // check for matches
         if (!neighborSmoot.isChecked && smoot.matchesWith(neighborSmoot)) {
 
           if (!matchingSmoots.includes(neighborSmoot)) {
             // check each one recursively
-            _this3.findNeighboringSmootMatches(neighborSmoot).forEach(function (smoot) {
+            _this4.findNeighboringSmootMatches(neighborSmoot).forEach(function (smoot) {
               return matchingSmoots.push(smoot);
             });
           }
@@ -964,7 +996,7 @@ var Board = function () {
 exports.default = Board;
 
 /***/ }),
-/* 5 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -976,7 +1008,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _my_canvas = __webpack_require__(6);
+var _my_canvas = __webpack_require__(7);
 
 var MyCanvas = _interopRequireWildcard(_my_canvas);
 
@@ -1147,7 +1179,7 @@ var Cannon = function () {
 exports.default = Cannon;
 
 /***/ }),
-/* 6 */
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -1177,7 +1209,7 @@ var getTopBorder = exports.getTopBorder = function getTopBorder() {
 };
 
 /***/ }),
-/* 7 */
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -1233,27 +1265,13 @@ var SmootSpace = function () {
 exports.default = SmootSpace;
 
 /***/ }),
-/* 8 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var getDistanceBetween2 = exports.getDistanceBetween2 = function getDistanceBetween2(pos1, pos2) {
-  return Math.sqrt(Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[1] - pos2[1], 2));
-};
-
-/***/ }),
 /* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _game_view = __webpack_require__(3);
+var _game_view = __webpack_require__(4);
 
 var _game_view2 = _interopRequireDefault(_game_view);
 
